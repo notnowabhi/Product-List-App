f7f7a26413a87e000bec373fdfb9b74c6eaf478a
notnowabhi <135256681+notnowabhi@users.noreply.github.com>
2024-08-09 00:25:48 +0530

    Create ProductAdapter.java

---
+++ ProductAdapter.java
@@ -0,0 +1,66 @@
+package com.example.newappproject;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    private List<Product> productList;
+    private Context context;
+
+    public ProductAdapter(List<Product> productList, Context context) {
+        this.productList = productList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_product, parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+
+        holder.titleTextView.setText(product.getTitle());
+        holder.priceTextView.setText("$" + product.getPrice());
+
+        Glide.with(context).load(product.getThumbnail()).into(holder.thumbnailImageView);
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, ProductDetailActivity.class);
+            intent.putExtra("product", product);
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        TextView titleTextView, priceTextView;
+        ImageView thumbnailImageView;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.titleTextView);
+            priceTextView = itemView.findViewById(R.id.priceTextView);
+            thumbnailImageView = itemView.findViewById(R.id.thumbnailImageView);
+        }
+    }
+}
